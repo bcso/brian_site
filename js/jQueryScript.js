a79ad57fe4e6f813f240051f3ebbb79dce825d39
bcso <bcso@uwaterloo.ca>
2014-11-10 18:19:11 -0500

    added nicer colours, buttons and transparency

---
+++ js/jQueryScript.js
@@ -0,0 +1,11 @@
+//$("#home").click(function () {
+//    $('html, body').animate({
+//        scrollTop: $("#home").offset().top
+//    }, 2000);
+//});
+
+
+var navBar = $("defaultNav");
+
+$(document).ready(function(){
+    navBar.fadeIn()});
