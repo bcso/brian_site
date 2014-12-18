314651d737df5cd1cc36b0afced51f4841594a4d
bcso <bcso@uwaterloo.ca>
2014-12-18 17:08:01 -0500

    playing around with fullPage.js, and jq ease

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $.material.init();
+    $("#mainNav").hide().fadeIn("100");
+	$('#home, #portfolio, #resume, #contact').hide().fadeIn("500");
+    
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+});
